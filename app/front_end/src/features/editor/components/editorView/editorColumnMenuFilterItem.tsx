4143b4f3253d40529afb0042972113e1100cc16e
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-12-18 10:51:49 +0200

    MDE/KFE-69 Populated operator dropdown

--- app/front_end/src/features/editor/components/editorView/editorColumnMenuFilterItem.tsx
+++ app/front_end/src/features/editor/components/editorView/editorColumnMenuFilterItem.tsx
@@ -1,10 +1,22 @@
 import { FilterAlt as FilterAltIcon } from '@mui/icons-material';
-import { Box, FormControl, InputLabel, MenuItem, OutlinedInput, Select, Typography, useTheme } from '@mui/material';
+import { Box, FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent, Typography, useTheme } from '@mui/material';
+import { FilterEnum } from '@/features/editor/types';
+import { SyntheticEvent, useState } from 'react';
 
-export interface EditorColumnMenuFilterItemProps {}
+export interface EditorColumnMenuFilterItemProps {
+    onClick: (event:  SyntheticEvent<Element, Event>) => void;
+    onFilter: (operator: FilterEnum) => void;
+}
 
 export const EditorColumnMenuFilterItem: React.FC<EditorColumnMenuFilterItemProps> = ({}) => {
   const Theme = useTheme();
+
+  const [operator, setOperator] = useState<FilterEnum>(FilterEnum.CONTAINS);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const operator = event.target.value as FilterEnum;
+    setOperator(operator);
+  };
 
   return (
     <Box
@@ -41,13 +53,36 @@ export const EditorColumnMenuFilterItem: React.FC<EditorColumnMenuFilterItemProp
           <Select
             id='filter-operator'
             label='Operator'
+            value={operator}
+            onChange={handleChange}
             size='small'
             sx={{
               fontSize: '0.8rem',
             }}
           >
-            <MenuItem>
-              ...
+            <MenuItem value={FilterEnum.CONTAINS}>
+              contains
+            </MenuItem>
+            <MenuItem value={FilterEnum.NOT_CONTAINS}>
+              does not contain
+            </MenuItem>
+            <MenuItem value={FilterEnum.EQUALS}>
+              equals
+            </MenuItem>
+            <MenuItem value={FilterEnum.NOT_EQUALS}>
+              does not equal
+            </MenuItem>
+            <MenuItem value={FilterEnum.STARTS}>
+              starts with
+            </MenuItem>
+            <MenuItem value={FilterEnum.ENDS}>
+              ends with
+            </MenuItem>
+            <MenuItem value={FilterEnum.EMPTY}>
+              is empty
+            </MenuItem>
+            <MenuItem value={FilterEnum.NOT_EMPTY}>
+              is not empty
             </MenuItem>
           </Select>
         </FormControl>
