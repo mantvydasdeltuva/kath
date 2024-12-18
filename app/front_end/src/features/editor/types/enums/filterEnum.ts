92c365f74afadf816d16acaa9ede38469c903113
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-12-18 10:51:07 +0200

    MDE/KFE-69 Implemented filter operator enum

---
+++ app/front_end/src/features/editor/types/enums/filterEnum.ts
@@ -0,0 +1,10 @@
+export enum FilterEnum {
+  CONTAINS = 'contains',
+  NOT_CONTAINS = 'does-not-contain',
+  EQUALS = 'equals',
+  NOT_EQUALS = 'does-not-equal',
+  STARTS = 'starts-with',
+  ENDS = 'ends-with',
+  EMPTY = 'is-empty',
+  NOT_EMPTY = 'is-not-empty',
+}
