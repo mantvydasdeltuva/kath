a99fd780af99a0ad5e827942aa8db9bc3a56eab7
Marius <privacyfirst555@proton.me>
2025-02-04 08:29:20 +0200

    Modifying vite.config.ts to allow access outside the docker container.

--- app/front_end/vite.config.ts
+++ app/front_end/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    host: '0.0.0.0', // Set this to 0.0.0.0 or true to listen on all addresses, including LAN and public addresses.
+    port: 5173,
+    origin: 'http://localhost:5173' // Defines the origin of the generated asset URLs during development.
+  }
 });
